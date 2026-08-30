f50cc7a7d3a2790d9f6dc4691860083432639df8
Ananda Putra N <nandakool@yahoo.co.id>
2026-08-30 04:38:16 +0000

    Aug 30, 2026, 11:38 AM

---
+++ data/translations.js
@@ -0,0 +1,13 @@
+const translations = {
+    en: {
+        navAbout: "About",
+        navProjects: "Projects",
+        contactLabel: "Contact"
+    },
+
+    id: {
+        navAbout: "Tentang",
+        navProjects: "Proyek",
+        contactLabel: "Kontak"
+    }
+};
