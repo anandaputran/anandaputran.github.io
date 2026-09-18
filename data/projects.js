54ae0f7f8a5515965ae0e2ed543ae1bd6a902c1c
Ananda Putra N <nandakool@yahoo.co.id>
2026-09-18 20:54:09 +0000

    Sep 19, 2026, 3:54 AM

--- data/projects.js
+++ data/projects.js
@@ -93,7 +93,7 @@ const projects = [
     image: "assets/img/projects/katga.webp",
     imageAlt: "KATGA internal K3 educational web game mockup",
 
-    tech: ["HTML", "CSS", "JavaScript", "Firebase"],
+    tech: ["HTML", "CSS", "JavaScript", "Firebase Firestore"],
 
     action: null,
   },
